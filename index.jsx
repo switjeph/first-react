7edbf6179934ef82828c2dccd94ed9ccf0fc4471
switjeph <aladinojephthah@gmail.com>
2025-02-24 14:17:23 +0100

    Changes made

--- index.jsx
+++ index.jsx
@@ -1,15 +1,51 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById('root'))
-root.render(
-    <main>
+
+function Header(){
+    return (
+      <header>
         <img src="react-logo.png" width="40px" alt="React logo" />
-        <h1>Fun fact about React by Jeph!</h1>
-        <ul>
-            <li>was first release in 2013!</li>
-            <li>was originally created by Jordan walke</li>
-            <li>Has well over 200k stars on GitHub</li>
-            <li>Is maintained by Meta </li>
-            <li>Powers thousands of enterprice apps, including mobile apps</li>
-        </ul>
-    </main>
+      </header>
+    );
+}
+
+function MainContent() {
+    return(
+        <main>
+           <h1>Why i'm excited to learn React!</h1>
+          <ol>
+            <li>It's a front-end library!</li>
+            <li>It makes building websites and mobile apps easy</li>
+            <li>It widly used library</li>
+            <li>Fun to code with</li>
+            <li>A roadmap to my career path!</li>
+          </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return(
+         <footer>
+            <small>@ 20204 Jeph  name here development. All rights reserved.</small>
+        </footer>
+      
+    )
+}
+
+function Page() {
+    return (
+      <>
+
+        <Header />
+        <MainContent/>
+        <Footer/>
+
+
+      </>
+    );
+}
+
+root.render(
+  <Page />
 )

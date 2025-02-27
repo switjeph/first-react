1b9294932a11470127a22f710f0c06dfcdcbf538
switjeph <aladinojephthah@gmail.com>
2025-02-27 13:47:46 +0100

    REACTFACT APP

--- Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="footer">
-        <small>@ 2024 Jeph name here development. All rights reserved.</small>
-      </footer>
-    );
-}
--- index.jsx
+++ index.jsx
@@ -1,21 +1,8 @@
 import { createRoot } from "react-dom/client";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+
+import App from "./App";
+
 const root = createRoot(document.getElementById('root'))
 
-function Page() {
-    return (
-      <>
-      
-        <Header />
-        <MainContent/>
-        <Footer/>
 
-      </>
-    );
-}
-
-root.render(
-  <Page />
-)
+root.render(<App/>)
